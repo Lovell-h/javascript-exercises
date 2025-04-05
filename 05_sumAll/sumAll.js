fb331cb82be93428ab432fa2ef87dcfd98bc9b1c
Lovell-h <205021015+Lovell-h@users.noreply.github.com>
2025-04-05 09:20:37 -0500

    passed all 7 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let total=0;
+    if (!Number.isInteger(start) ||!Number.isInteger(end)) return "ERROR";
+    if (start < 0 || end < 0) return "ERROR";
+    if (start < end)
+        {
+            for (i=start; i<=end;i++)
+                {
+                    total=total+i;
+                }
+        } else
+        {
+            for (i=end; i<=start;i++)
+                {
+                    total=total+i;
+                }
+        }
+        
+    return total;
+    
 };
 
 // Do not edit below this line
