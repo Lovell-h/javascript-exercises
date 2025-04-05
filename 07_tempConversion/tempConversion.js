7e503a9500ea69e1de514e7b941a468536275975
Lovell-h <205021015+Lovell-h@users.noreply.github.com>
2025-04-05 10:20:44 -0500

    passed all 6

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let results = Math.round(((temp-32)/1.8)*10)/10;
+  return results;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  
+  let results = Math.round(((temp * 1.8)+32)*10)/10;
+  return results;
 };
 
 // Do not edit below this line
