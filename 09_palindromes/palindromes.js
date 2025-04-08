9b3cee79f09a1e5dc1cba62bd78bd748be631076
Lovell-h <205021015+Lovell-h@users.noreply.github.com>
2025-04-08 12:45:54 -0500

    passed all 8

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (inString) {
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const newString = inString
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+const reverseString = newString.split('').reverse().join('');
+return newString === reverseString;
 };
 
 // Do not edit below this line
